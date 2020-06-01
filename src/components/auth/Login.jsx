0dbe519f9fbcfc7cdf5d805e771d2951d795a03d
Alkard000 <aburtoivan620@gmail.com>
2020-05-31 22:07:57 -0400

    Creando el login y el Crear Cuenta

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+    const [usuario, setUsuario] = useState({
+        email : '',
+        password : ''
+    })
+
+    //EXTRAER
+    const {email, password} = usuario;
+
+    const handleChange = e => {
+        setUsuario({
+            ...usuario, 
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //CUANDO SE INCIA SESION
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        //VALIDAR PARA QUE NO HAYA CAPOS VACIOS
+
+
+        //PASARLO A LA ACTION
+    }
+
     return (  
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesion</h1>
+
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesion"
+                        />
+                    </div>
+                </form>
+
+                <Link to={'/nueva-cuenta'} className='enlace-cuenta'>
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
     );
 }
  
--- src/components/auth/NuevaCuenta.jsx
+++ src/components/auth/NuevaCuenta.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, {useState} from 'react';
+
+import {Link} from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+    const [usuario, setUsuario] = useState({
+        nombre : '',
+        email : '',
+        password : '',
+        confirmar : ''
+    })
+
+    //EXTRAER
+    const {nombre, email, password, confirmar} = usuario;
+
+    const handleChange = e => {
+        setUsuario({
+            ...usuario, 
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //CUANDO SE INCIA SESION
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        //VALIDAR PARA QUE NO HAYA CAPOS VACIOS
+
+
+        //PASARLO A LA ACTION
+    }
+
     return (  
-        <h1>Desde NuevaCuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Obtener una Cuenta</h1>
+
+                <form
+                    onSubmit={handleSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input 
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Tu Nombre"
+                            value={nombre}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu Email"
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tu Password"
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="confirmar">Repetir Password</label>
+                        <input 
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            placeholder="Repite Tu Password"
+                            value={confirmar}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input 
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Registrarme"
+                        />
+                    </div>
+                </form>
+
+                <Link to={'/'} className='enlace-cuenta'>
+                    Volver a Iniciar Sesion
+                </Link>
+            </div>
+        </div>
     );
 }
  
